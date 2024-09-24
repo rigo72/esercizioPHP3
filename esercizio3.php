aefce42365d204c05be31a0c6ff61aa1c2aaec7a
rigo72 <mattia.gori27@gmail.com>
2024-09-24 11:26:18 +0200

    Commit 1

---
+++ esercizio3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $min = 16;
+        $max = 60;
+        $ris = rand($min, $max);
+        echo "<p style ='font-size:" . $ris . "px'>Hello World!</p>"
+    
+    
+    
+    ?>
+</body>
+</html>
